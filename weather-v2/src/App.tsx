3e45af6991dfde29591791679928133b4af6f14c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-04-03 02:11:17 +0300

    Update App.tsx

--- weather-v2/src/App.tsx
+++ weather-v2/src/App.tsx
@@ -16,7 +16,8 @@ function App() {
   const getData = async (city: string) => {
     try {
       setLoading(true)
-      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${import.meta.env.VITE_KEY_API}`)
+      // const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${import.meta.env.VITE_KEY_API}`)
+      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=47d7b942ea7e702e4bdd7a72a9fc3f75`)
       console.log(data)
       return setWeather(data)
     } catch (error) {
